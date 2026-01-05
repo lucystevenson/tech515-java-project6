d09abf647c9fa16b5970c13959b303eee87af847
lucystevenson <lucyannestevenson7@gmail.com>
2026-01-05 14:12:12 +0000

    Set up repo

--- library-java17-mysql-app/LibraryProject2/src/main/java/com/sparta/mg/libraryproject2/exceptions/AuthorNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.mg.libraryproject2.exceptions;
-
-public class AuthorNotFoundException extends Exception{
-    public AuthorNotFoundException(String name) {
-        super("Could not find Author:" + name);
-    }
-}
